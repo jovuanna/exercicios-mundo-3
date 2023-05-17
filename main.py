911915929e9dba9e2d4203cc8308b39805590109
Giovanna Giselli Santana de Oliveira <113366850+jovuanna@users.noreply.github.com>
2023-05-16 22:33:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+lanches = ('água','hamburguer', 'suco', 'pizza', 'pudim')
+print(lanches)
+sorted(lanches) #coloca os elementos em ordem
+# for c in lanches:
+#   print(c[0])
+# tuplas são imutáveis, porém, listas não.
+'''lista = ["hamburguer", "suco", "pizza", "pudim"]
+lista[2] = " ovo"
+print(lista)'''
